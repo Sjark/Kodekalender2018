553a3865b182e84c2fa8254880c03b6ed751d99d
Lasse Meland <sjark89@gmail.com>
2018-12-02 13:00:45 +0100

    Dag 1

---
+++ Dag1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Dag1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = File.ReadAllLines("Input\\input-vekksort.txt");
+            var result = new List<int>();
+            var num1 = 0;
+
+            for (int i = 0; i < file.Length; i++)
+            {
+                var num2 = int.Parse(file[i]);
+
+                if (num2 < num1)
+                {
+                    continue;
+                }
+
+                num1 = num2;
+                result.Add(num2);
+            }
+
+            Console.WriteLine(result.Sum());
+            Console.Read();
+        }
+    }
+}
