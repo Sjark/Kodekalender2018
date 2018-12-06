7c0bfed24fa413587588fe58669b54b326db6cad
Lasse Meland <sjark89@gmail.com>
2018-12-06 07:54:12 +0100

    Dag6

---
+++ Dag6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dag6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nullHeavyNumbers = new List<long>();
+
+            for (long i = 1; i <= 18163106; i++)
+            {
+                var iAsString = i.ToString();
+
+                if (iAsString.Count(a => a == '0') > iAsString.Count(a => a != '0'))
+                {
+                    nullHeavyNumbers.Add(i);
+                }
+            }
+
+            Console.WriteLine(nullHeavyNumbers.Sum());
+            Console.Read();
+        }
+    }
+}
