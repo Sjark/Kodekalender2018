0103ae83beb8ac74bc4ad055a98358bfae48c30d
Lasse Meland <sjark89@gmail.com>
2018-12-15 12:17:58 +0100

    Dag15

---
+++ Dag15/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Dag15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("Input\\input-gullbursdag.txt")
+                .Select(a => int.Parse(a.Substring(a.IndexOf("f.") + 2)))
+                .ToList();
+
+            var result = 0;
+
+            foreach (var year in input)
+            {
+                var i = 1;
+
+                while (true)
+                {
+                    var goldYear = year + i;
+                    var ageSquared = (int)Math.Pow(i, 2);
+
+                    if (ageSquared == goldYear)
+                    {
+                        result += 1;
+                        break;
+                    }
+
+                    if (ageSquared > goldYear)
+                    {
+                        break;
+                    }
+
+                    i++;
+                }
+            }
+            
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
