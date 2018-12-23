821703b32fbe3ae0a778f65a2b979ca15540fb68
Lasse Meland <sjark89@gmail.com>
2018-12-23 12:12:24 +0100

    Dag 22, not solved, was stuck to long trying to solve it with TSP

---
+++ Dag22/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Dag22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("Input\\input-luke-22.txt")
+                .Select(a =>
+                {
+                    var coords = a.Split(",");
+
+                    return new Coord(decimal.Parse(coords[0].Replace('.', ',')), decimal.Parse(coords[1].Replace('.', ',')));
+                }).ToList();
+
+            Console.Read();
+        }
+    }
+
+    public class Coord
+    {
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+
+        public Coord(decimal x, decimal y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
